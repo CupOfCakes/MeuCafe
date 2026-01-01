b26c1a4f269e7a935c7a35d4dfdd21a98298cb85
CupOfCakes <heitor.scarv@gmail.com>
2025-12-31 21:57:44 -0300

    remove: rote for seach by id and email / feat: rote for return all users

---
+++ Infrastructure/DependencyInjection/InfrastructureModule.cs
@@ -0,0 +1,27 @@
+using Infrastructure.Persistence.Context;
+using Domain.Repositories;
+using Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DependencyInjection;
+
+public static class InfrastructureModule
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("Default")
+                )
+        );
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
--- Infrastructure/Persistence/Context/AppDbContext.cs
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -1,5 +1,25 @@
+using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace Infrastructure.Persistence.Context;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<User> Users => Set<User>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AppDbContext).Assembly
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
+
 }
