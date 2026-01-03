b6ebea4ff1c6e8f0c908fa47cf196c84a578df12
CupOfCakes <heitor.scarv@gmail.com>
2026-01-03 10:51:01 -0300

    refactor: change route url to api/requests and user class to clients; feat: add AllClients route

--- API/Controllers/RequestsController.cs
+++ API/Controllers/RequestsController.cs
@@ -1,12 +1,24 @@
-using Application.UseCases.Users;
+using Application.UseCases.Clients;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeuCafe.Controllers;
 
 [ApiController]
-[Route("api/{controller}")]
+[Route("api/requests")]
 public class RequestsController : ControllerBase
 {
-    private readonly ListUsersUseCase _listUsersUseCase;
+    private readonly ListClientsUseCase _listClientsUseCase;
     
+    public RequestsController(ListClientsUseCase listClientsUseCase)
+    {
+        _listClientsUseCase = listClientsUseCase;
+    }
+
+    [HttpGet("AllClients")]
+    public async Task<IActionResult> GetAllClients()
+    {
+        var result = await _listClientsUseCase.ExecuteAsync();
+        return Ok(result);
+    }
+
 }
