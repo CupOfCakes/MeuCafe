af491fe255a663936ead087ec82b91487edf335b
CupOfCakes <heitor.scarv@gmail.com>
2026-01-08 16:38:17 -0300

    feat: new constructor

--- Application/UseCases/Clients/Create/ClientCreatedDTO.cs
+++ Application/UseCases/Clients/Create/ClientCreatedDTO.cs
@@ -2,4 +2,9 @@ namespace Application.UseCases.Clients.Create;
 
 public class ClientCreatedDTO{
     public string URL = string.Empty;
+
+    public ClientCreatedDTO(string url)
+    {
+        URL = url;
+    }
 }
