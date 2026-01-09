3b02070ecc839fff541a8529c45d88f4de30a89e
CupOfCakes <heitor.scarv@gmail.com>
2026-01-09 14:02:27 -0300

    feat: add domain exceptions

---
+++ Domain/Exceptions/EmptyPasswordException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Domain.Exceptions;
+
+namespace Domain.Exceptions;
+
+public sealed class EmptyPasswordException
+{
+	public EmptyPasswordException()
+		: base("Empty client password") { }
+}
---
+++ Domain/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Domain.Exceptions;
+
+namespace Domain.Exceptions;
+
+public sealed class InvalidEmailException
+{
+	public InvalidEmailException()
+		: base("Invalid client Email") { }
+}
