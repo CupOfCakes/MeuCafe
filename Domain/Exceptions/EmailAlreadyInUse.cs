37a9c224d4cb19e060758365299519d03e796070
CupOfCakes <heitor.scarv@gmail.com>
2026-01-09 18:25:41 -0300

    feat: new exceptions

---
+++ Domain/Exceptions/EmailAlreadyInUse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Domain.Exceptions;
+
+namespace Domain.Exceptions;
+
+public sealed class EmailAlreadyInUse : DomainException
+{
+	public EmailAlreadyInUse()
+		: base("Email alreay in use") { }
+}
