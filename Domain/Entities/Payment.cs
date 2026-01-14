924e221cc4aaf1592640bc3ad45b873e21114f71
CupOfCakes <heitor.scarv@gmail.com>
2026-01-14 17:59:13 -0300

    feat: new class Payment on Domain for table on database

---
+++ Domain/Entities/Payment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid SenderId { get; set; }
+        public Guid RecipientId { get; set; }
+        public decimal Value { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string PixTxId { get; set; } = string.Empty;
+        public string QrCodePayload { get; set; } = string.Empty;
+        public DateTimeOffset ExpiresAt { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset? ConfirmedAt { get; set; }
+        public DateTimeOffset? ReversedAt { get; set; }
+        public string? UrlProof { get; set; }
+    }
+}
