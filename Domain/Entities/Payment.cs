eb39ab05b6e2e2d5d987c2ec8e7901d5be986e68
CupOfCakes <heitor.scarv@gmail.com>
2026-01-14 20:41:17 -0300

    feat: add new payment table

--- Domain/Entities/Payment.cs
+++ Domain/Entities/Payment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,17 @@ namespace Domain.Entities
 {
     public class Payment
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public Guid SenderId { get; set; }
+        public required Client Sender { get; set; }
         public Guid RecipientId { get; set; }
+        public required Client Recipient { get; set; }
         public decimal Value { get; set; }
-        public string Status { get; set; } = string.Empty;
+        public PaymentStatus Status { get; set; } = PaymentStatus.PENDING;
         public string PixTxId { get; set; } = string.Empty;
         public string QrCodePayload { get; set; } = string.Empty;
         public DateTimeOffset ExpiresAt { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
         public DateTimeOffset? ConfirmedAt { get; set; }
         public DateTimeOffset? ReversedAt { get; set; }
         public string? UrlProof { get; set; }
