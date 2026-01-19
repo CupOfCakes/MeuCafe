f753649d0b06a0fb6d8aa60808fc09baf3d69df6
CupOfCakes <heitor.scarv@gmail.com>
2026-01-19 10:36:50 -0300

    feat: add warmup route and system controller

---
+++ Infrastructure/Persistence/Repositories/SystemRepository.cs
@@ -0,0 +1,26 @@
+﻿using Application.Repositories;
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class SystemRepository : IWarmupService
+    {
+        private readonly AppDbContext _context;
+
+        public SystemRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            await _context.Database.ExecuteSqlRawAsync("SELECT 1");
+        }
+    }
+}
