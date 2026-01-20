4568a70d6ca4ffbffea38c40758afab0e3755281
CupOfCakes <heitor.scarv@gmail.com>
2026-01-20 14:46:48 -0300

    feat: add payment repository and AnyPendingByClientId function

---
+++ Infrastructure/Persistence/Repositories/PaymentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Repositories;
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PaymentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AnyPendingByClientId(Guid id)
+        {
+            bool pending = await _context.Payments
+            .AnyAsync(p => p.SenderId == id && p.Status == Domain.Enums.PaymentStatus.PENDING);
+
+            return pending;
+        }
+    }
+}
