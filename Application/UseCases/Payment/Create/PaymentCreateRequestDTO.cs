e7fd1dd30736eb31b73203c2c6d15c8123c40f29
CupOfCakes <heitor.scarv@gmail.com>
2026-02-11 16:43:12 -0300

    feat: payment rote base

---
+++ Application/UseCases/Payment/Create/PaymentCreateRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Payment.Create
+{
+    internal class PaymentCreateRequestDTO
+    {
+        public Guid RecipientId { get; set; }
+        public decimal value { get; set; }
+    }
+}
---
+++ Application/UseCases/Payment/Create/PaymentResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Payment.Create
+{
+    internal class PaymentResponseDTO
+    {
+        public int PaymentId { get; set; }
+        public string QrCodePayload { get; set; } = string.Empty;
+        public DateTimeOffset ExpiresAt { get; set; } = DateTimeOffset.UtcNow;
+        public PaymentStatus Status { get; set; } = PaymentStatus.PENDING;
+    }
+}
